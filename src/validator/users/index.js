6fe7e9a953685ad566f6b474f6902ba637dea0aa
M. Afwan <mafwan.dev@sumbawabaratkab.go.id>
2025-02-05 06:59:32 +0800

    user registration implemented

---
+++ src/validator/users/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantEerror');
+const { UserPayloadSchema } = require('./schema');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validationResult = UserPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
